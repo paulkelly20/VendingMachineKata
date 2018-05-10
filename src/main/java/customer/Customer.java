ed2e85df7971d27b93eaad045e89c2faa9554b9f
Paul Kelly <paul.kelly20@gmail.com>
2018-05-10 08:46:19 +0100

    Test for customer wallet and pocket passes

---
+++ src/main/java/customer/Customer.java
@@ -0,0 +1,38 @@
+package customer;
+
+import coins.Coin;
+import products.Product;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private String name;
+    private ArrayList<Coin> wallet;
+    private ArrayList<Product> pocket;
+
+    public Customer(String name, ArrayList<Coin> wallet) {
+        this.name = name;
+        this.wallet = wallet;
+        this.pocket = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int countCoinsInWallet() {
+        return wallet.size();
+    }
+
+    public int getProductCount() {
+        return pocket.size();
+    }
+
+    public void addCoinToWallet(Coin coin){
+        this.wallet.add(coin);
+    }
+
+    public void addProductToPocket(Product product){
+        this.pocket.add(product);
+    }
+}
---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,43 @@
+import coins.Coin;
+import coins.CoinType;
+import customer.Customer;
+import machine.CoinSlot;
+import org.junit.Before;
+import org.junit.Test;
+import products.Product;
+import products.Sweet;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+    Customer customer;
+    CoinSlot coinslot;
+    Coin onePound;
+    Coin fiftyPence;
+    Product product;
+
+    @Before
+    public void before(){
+        coinslot = new CoinSlot();
+        onePound = new Coin(CoinType.ONEPOUND);
+        fiftyPence = new Coin(CoinType.FIFTYPENCE);
+        ArrayList<Coin> wallet = new ArrayList<>();
+        wallet.add(onePound);
+        wallet.add(fiftyPence);
+        customer = new Customer("Paul",wallet );
+        product = new Sweet("Mars", 0.50);
+
+    }
+
+    @Test
+    public void testCoinsInWallet() {
+        assertEquals(2, customer.countCoinsInWallet());
+    }
+
+    @Test
+    public void customerHasEmptyPocket() {
+            assertEquals(0, customer.getProductCount());
+    }
+}
